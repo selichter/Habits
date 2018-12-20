195e45bc6f8efbf7ad7b035bde956e005bf2fc5d
selichter <lichter.sarahe@gmail.com>
2018-12-19 21:16:03 -0600

    project setup

---
+++ Habits/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Habits
+//
+//  Created by Sarah Lichter on 12/19/18.
+//  Copyright © 2018 Sarah Lichter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Habits' do
+  use_frameworks!
+
+  pod 'SwiftLint'
+
+  target 'HabitsTests' do
+    inherit! :search_paths
+    pod 'Quick'
+  end
+
+  target 'HabitsUITests' do
+    inherit! :search_paths
+  end
+
+end
