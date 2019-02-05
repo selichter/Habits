3d46e257665eecd73c84e4b83a9aaf925f225daf
selichter <lichter.sarahe@gmail.com>
2019-02-04 22:14:37 -0600

    mend

--- Habits/NewHabit/NewHabitViewController.swift
+++ Habits/NewHabit/NewHabitViewController.swift
@@ -13,16 +13,7 @@ extension UIViewController {
 
 class NewHabitViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var timePeriodStackView: UIStackView!
-//    let colorChoices = allColors
-
-//    @IBOutlet weak var colorChoiceStackView: UIStackView!
-    
     @IBOutlet weak var habitFormView: HabitForm!
-//
-//
-//    var timePeriodValue = String()
-//    var colorChoice = String()
     
     @IBAction func createHabit(_ sender: Any) {
         let newHabit = HabitEntity(habitId: UUID().uuidString,
@@ -37,10 +28,6 @@ class NewHabitViewController: UIViewController, UITextFieldDelegate {
         performSegueToReturnBack()
     }
     
-//    var timePeriodChoices: [String] {
-//        return TimePeriod.allCases.map({$0.rawValue})
-//    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         habitFormView.xibHabitNameInput.delegate = self
@@ -50,68 +37,6 @@ class NewHabitViewController: UIViewController, UITextFieldDelegate {
         habitFormView.xibHabitNameInput.placeholder = "HABIT NAME"
         habitFormView.xibTargetInput.placeholder = "4"
         habitFormView.xibMeasurementInput.placeholder = "DAILY"
-
-//        for choice in timePeriodChoices {
-//            timePeriodStackView.addArrangedSubview(makeButtonsWithText(choice))
-//        }
-//
-//        for choice in Array(colorChoices.keys) {
-//            colorChoiceStackView.addArrangedSubview(makeColorButtons(choice))
-//        }
     }
-    
-   
-    
-    
-//    func makeButtonsWithText(_ text: String)-> UIButton {
-//        let timePeriodButton = UIButton(type: UIButton.ButtonType.system)
-//        let width = timePeriodButton.intrinsicContentSize.width
-//        timePeriodButton.frame = CGRect(x: 20, y: 20, width: width, height: 100)
-//        timePeriodButton.setTitle(text, for: .normal)
-//        timePeriodButton.setTitleColor(UIColor.black, for: .normal)
-//        timePeriodButton.layer.cornerRadius = 10
-//        timePeriodButton.sizeToFit()
-//        timePeriodButton.addTarget(self, action: #selector(selectButtonValue), for: .touchUpInside)
-//
-//        return timePeriodButton
-//    }
-//
-//    func makeColorButtons(_ color: String) -> UIButton {
-//        let colorChoiceButton = UIButton(type: UIButton.ButtonType.system)
-//        colorChoiceButton.backgroundColor = allColors[color]!.primary
-//        colorChoiceButton.accessibilityIdentifier = color
-//
-//        let width = colorChoiceButton.intrinsicContentSize.width
-//        colorChoiceButton.frame = CGRect(x: 20, y: 20, width: width, height: 10)
-//        colorChoiceButton.layer.masksToBounds = true
-//        colorChoiceButton.layer.cornerRadius = colorChoiceButton.frame.width/2
-//
-//        colorChoiceButton.addTarget(self, action: #selector(selectColor), for: .touchUpInside)
-//        return colorChoiceButton
-//
-//    }
-    
-//    @IBAction func selectButtonValue(sender:UIButton){
-//        for button in timePeriodStackView.subviews {
-//            if button is UIButton {
-//                button.backgroundColor = UIColor.white
-//            }
-//        }
-//        sender.backgroundColor = UIColor.lightGray
-//        timePeriodValue = sender.titleLabel!.text ?? "daily"
-//    }
-//
-//    @IBAction func selectColor(sender:UIButton){
-//        for button in colorChoiceStackView.subviews {
-//            if button is UIButton {
-//                button.layer.borderWidth = 0
-//                button.layer.borderColor = UIColor.white.cgColor
-//            }
-//        }
-//        sender.layer.borderColor = UIColor.black.cgColor
-//        sender.layer.borderWidth = 2
-//        colorChoice = sender.accessibilityIdentifier ?? "cyan"
-//    }
-    
     
 }
